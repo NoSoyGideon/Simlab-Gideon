ef113fd3ac07b308b20e8770188844e6c449e3a7
NoSoyGideon <163026488+NoSoyGideon@users.noreply.github.com>
2025-10-09 13:29:25 -0400

    que tonto

--- simulador/settings.py
+++ simulador/settings.py
@@ -133,4 +133,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-ALLOWED_HOSTS['localhost','simuladorcircunvalacionuno.onrender.com']
+
+ALLOWED_HOSTS = ['localhost','simuladorcircunvalacionuno.onrender.com']
